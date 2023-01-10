1259159817011c1297f9d635e1bbe9fd15562edc
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-11 00:39:02 +0100

    Refactoring

--- index.js
+++ index.js
@@ -16,8 +16,49 @@ const client = new Client({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME, 
     password: process.env.DB_PASSWORD,
-    port: DB_PORT,
+    port: 5432,
 });
+
+function faillingId(id){
+    return Number.isNaN(Number(id)) ||  id % 1 !== 0 || typeof id === typeof Boolean()
+}
+
+function faillingMessage(message){
+    return message !== undefined  && typeof message != typeof String()
+}
+
+function faillingDone(done){
+    return done !== undefined && typeof done != typeof Boolean()
+}
+/**
+ * @status      => 500
+ * @message     => "Erreur serveur"
+ * @data        => undefined
+ */
+class Responcer {
+    constructor(conf = {}) {
+        this.status = conf.status       ||  500
+        this.message = conf.message     ||  "Erreur serveur"
+        this.data = conf.status         ||  undefined
+    }
+
+    statusStr(){
+        return (200 <= this.status && this.status <300) ? "SUCCESS" : "FAIL"
+    }
+
+    send(response){
+        return response.status(this.status).json({
+            status: this.statusStr(),
+            message: this.message,
+            data: this.data
+        })
+    }
+
+    info(){
+        return `${this.status} | ${this.statusStr()}\n${this.message}`
+    }
+}
+
 
 client.connect();
 
@@ -41,328 +82,270 @@ app.use(function (req, res, next) {
     next();
 });
 
+
 // ROUTES -----------------------
 
 // Récupération des Users
-app.get('/api/users', async (req , res) => {
-    
-    try {
+app.get('/api/users', async (req , res) => 
+{
+    let rpcr = new Responcer()
+
+    try 
+    {
         const data = await client.query('SELECT id, username FROM users');
-        res.status
-        res.status(200).json({
-            status: "SUCCESS",
-            message: `Récupération de ${data.rowCount} utilisateurs`,
-            data: data.rows
-        });
-        console.log(`get | /api/users | 200 | SUCCESS\nRécupération de ${data.rowCount} utilisateurs`);
+        
+        rpcr.status = 200 ;
+        rpcr.message = `Récupération de ${data.rowCount} utilisateurs` ;
+        rpcr.data = data.rows ;
+        
     }
-    catch (err) {
-        res.status(500).json({
-            status: "FAIL",
-            message: err.stack,
-            data : undefined
-        });
-        console.log(`get | /api/users | 500 | FAIL\n${err.stack}`);
+    catch (err) 
+    {
+        console.log(err.stack);
     }
+    console.log(`get | /api/users | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
 // Récupération des Tickets
-app.get('/api/tickets', async (req, res) => {
-    try {
+app.get('/api/tickets', async (req, res) => 
+{
+    let rpcr = new Responcer()
+    
+    try 
+    {
         const data = await client.query('SELECT * FROM tickets');
-        res.status(200).json({
-            status: "SUCCESS",
-            message: `Récupération de ${data.rowCount} tickets`,
-            data: data.rows
-        });
-        console.log(`get | /api/tickets | 200 | SUCCESS\nRécupération de ${data.rowCount} tickets`);
+
+        rpcr.status = 200 ;
+        rpcr.message = `Récupération de ${data.rowCount} tickets` ;
+        rpcr.data = data.rows ;
     }
-    catch (err) {
+    catch (err) 
+    {
         console.log(err.stack)
-        res.status(500).json({
-            status: "FAIL",
-            message: err.stack,
-            data : undefined
-        });
-        console.log(`get | /api/tickets | 500 | FAIL\n${err.stack}`);
     }
+    console.log(`get | /api/tickets | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
+
 // Récupération d'un Ticket avec son id
-app.get('/api/tickets/:id', async (req, res) => {
+app.get('/api/tickets/:id', async (req, res) => 
+{
+    let rpcr = new Responcer()
     const id = req.params.id
     
     // Vérifiction du Type de l'id entrante
-    if (
-        Number.isNaN(Number(id)) || 
-        id % 1 !== 0 ||
-        typeof id === typeof Boolean()
-    )
+    if (faillingId(id))
     {
-        res.status(404).json({
-            status: "FAIL",
-            message: `${id} n'est pas un nombre entier`,
-            data : undefined
-        });
-            console.log(`get | /api/tickets/${id} | 404 | FAIL\n${id} n'est pas un nombre`);
-        return ;
-    }
+        rpcr.status = 400 ;
+        rpcr.message = `${id} n'est pas un nombre entier` ;
+    } 
+    else 
+    {
+        try 
+        {
+            const data = await client.query('SELECT * FROM tickets WHERE id=$1', [id]);
+    
+            // Vérifiction de l'existence de l'id
+            if (data.rows.length === 1) {
+                
+                rpcr.status = 200 ;
+                rpcr.message = `Récupération du ticket ${id}` ;
+                rpcr.data = data.rows[0] ;
 
-    try {
-        const data = await client.query('SELECT * FROM tickets WHERE id=$1', [id]);
-
-        // Vérifiction de l'existence de l'id
-        if (data.rows.length === 1) {
-            res.status(200).json({
-                status: "SUCCESS",
-                message: `Récupération du ticket ${id}`,
-                data: data.rows[0]
-            });
-            console.log(`get | /api/tickets/${id} | 200 | FAIL\nRécupération du ticket ${id}`);
-        } else {
-            res.status(404).json({
-                status: "FAIL",
-                message: `Le ticket ${id} n'existe pas`,
-                data : undefined
-            });
-            console.log(`get | /api/tickets/${id} | 404 | FAIL\nLe ticket ${id} n'existe pas`);
+            } else {
+                rpcr.status = 404 ;
+                rpcr.message = `Le ticket ${id} n'existe pas` ;
+            }
+        }
+        catch (err) 
+        {
+            console.log(err.stack)
         }
     }
-    catch (err) {
-        console.log(err.stack)
-        res.status(500).json({
-            status: "FAIL",
-            message: err.stack,
-            data : undefined
-        });
-        console.log(`get | /api/tickets/${id} | 500 | FAIL\n${err.stack}`);
-    }
+    console.log(`get | /api/tickets/${id} | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
+
 // Création d'un Ticket
-app.post('/api/tickets', async (req, res) => {
+app.post('/api/tickets', async (req, res) => 
+{
+    let rpcr = new Responcer()
     const { user_id, message} = req.body;
 
     // Vérifiction du Type du user_id entrant
-    if (
-        Number.isNaN(Number(user_id)) || 
-        user_id % 1 !== 0 ||
-        typeof user_id === typeof Boolean()
-    )
+    if (faillingId(user_id))
     {
-        res.status(404).json({
-            status: "FAIL",
-            message: `${user_id} n'est pas un nombre entier`,
-            data : undefined
-        });
-            console.log(`post | /api/tickets | 404 | FAIL\n${user_id} n'est pas un nombre`);
-        return ;
-    }
+        rpcr.status = 400 ;
+        rpcr.message = `${user_id} n'est pas un nombre entier` ;
+    } 
+    else if (faillingMessage(message))
+    {
+        rpcr.status = 400 ;
+        rpcr.message = `${message} n'est pas un texte` ;
+    } 
+    else 
+    {
+        try 
+        {
+            // Vérifiction de la presence des paramètres nécessaires
+            if (message !== undefined  && user_id !== undefined ) 
+            {
+                const usersList = await client.query('SELECT id FROM users WHERE id = $1',[user_id]);
 
-    // Vérifiction du Type du message entrant
-    if (message !== undefined  && typeof message != typeof String()){
-        res.status(404).json({
-            status: "FAIL",
-            message: `${message} n'est pas un texte`,
-            data : undefined
-        });
-        console.log(`post | /api/tickets | 404 | FAIL\n${message} n'est pas un texte`);
-        return ;
-    }
+                // Vérifiction de l'existence du user_id
+                if (usersList.rowCount === 1)
+                {
+                    const data = await client.query('INSERT INTO tickets (message, user_id) VALUES ($1,$2) RETURNING *', [message, user_id]);
 
-    try {
-        
-        // Vérifiction de la presence des paramètres nécessaires
-        if (message !== undefined  && user_id !== undefined ) {
-            const usersList = await client.query('SELECT id FROM users WHERE id = $1',[user_id]);
-
-            // Vérifiction de l'existence du user_id
-            if (usersList.rowCount === 1){
-                const data = await client.query('INSERT INTO tickets (message, user_id) VALUES ($1,$2) RETURNING *', [message, user_id]);
-                res.status(201).json({
-                    status: "SUCCESS",
-                    message: `Création du ticket ${data.rows[0].id}`,
-                    data: data.rows[0]
-                });
-                console.log(`post | /api/tickets | 200 | SUCCESS\nCréation du ticket ${data.rows[0].id}`);
-            } else {
-                res.status(400).json({
-                    status: "FAIL",
-                    message: `L'utilisateur ${user_id} n'existe pas`,
-                    data : undefined
-                });
-                console.log(`post | /api/tickets | 400 | FAIL\nStructure incorrect : { message : string , user_id : number }`);
+                    rpcr.status = 201 ;
+                    rpcr.message = `Création du ticket ${data.rows[0].id}` ;
+                    rpcr.data = data.rows[0] ;
+                } 
+                else 
+                {
+                    rpcr.status = 404 ;
+                    rpcr.message = `L'utilisateur ${user_id} n'existe pas` ;
+                }
+            } 
+            else 
+            {
+                rpcr.status = 400 ;
+                rpcr.message = "Structure incorrect : { message : string , user_id : number }" ;
             }
-
-        } else {
-            res.status(400).json({
-                status: "FAIL",
-                message: "Structure incorrect : { message : string , user_id : number }",
-                data : undefined
-            });
-            console.log(`post | /api/tickets | 400 | FAIL\nStructure incorrect : { message : string , user_id : number }`);
         }
-
+        catch (err) 
+        {
+            console.log(err.stack)
+        }
     }
-    catch (err) {
-        res.status(500).json({
-            status: "FAIL",
-            message: err.stack,
-            data : undefined
-        });
-        console.log(`post | /api/tickets/${id} | 500 | FAIL\n${err.stack}`);
-    }
+    console.log(`post | /api/tickets | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
 // Modification d'un Ticket
-app.put('/api/tickets', async (req, res) => {
-
+app.put('/api/tickets', async (req, res) => 
+{
+    let rpcr = new Responcer()
     const { id, message, done} = req.body;
 
     // Vérifiction de la presence des paramètres nécessaires
-    if (!(id !== undefined && (message !== undefined || done !== undefined))) {
-        res.status(400).json({
-            status: "FAIL",
-            message: "Structure incorrect : { id : number , message : string , done : boolean } ou { id : number , message : string } ou  { id : number , done : boolean }",
-            data : undefined
-        });
-        console.log(`put | /api/tickets | 400 | FAIL\nStructure incorrect : { id : number , message : string , done : boolean } ou { id : number , message : string } ou  { id : number , done : boolean }`);
-        return ;
+    if (!(id !== undefined && (message !== undefined || done !== undefined))) 
+    {
+        rpcr.status = 400 ;
+        rpcr.message = "Structure incorrect : { id : number , message : string , done : boolean } ou { id : number , message : string } ou  { id : number , done : boolean }" ;
     }
     // Vérifiction du Type de l'id entrante
-    if (
-        Number.isNaN(Number(id)) || 
-        id % 1 !== 0 ||
-        typeof id === typeof Boolean()
-    )
+    else if (faillingId(id))
     {
-        res.status(404).json({
-            status: "FAIL",
-            message: `${id} n'est pas un nombre entier`,
-            data : undefined
-        });
-            console.log(`put | /api/tickets | 404 | FAIL\n${id} n'est pas un nombre`);
-        return ;
+        rpcr.status = 400 ;
+        rpcr.message = `${id} n'est pas un nombre entier` ;
     }
-
     // Vérifiction du Type du message entrant
-    if (message !== undefined && typeof message != typeof String()){
-        res.status(404).json({
-            status: "FAIL",
-            message: `${message} n'est pas un texte`,
-            data : undefined
-        });
-        console.log(`put | /api/tickets | 404 | FAIL\n${message} n'est pas un texte`);
-        return ;
+    else if (faillingMessage(message))
+    {
+        rpcr.status = 400 ;
+        rpcr.message = `${message} n'est pas un texte` ;
     }
 
     // Vérifiction du Type du done entrant
-    if (done !== undefined && typeof done != typeof Boolean()){
-        res.status(404).json({
-            status: "FAIL",
-            message: `${done} n'est pas un booleen`,
-            data : undefined
-        });
-        console.log(`put | /api/tickets | 404 | FAIL\n${message} n'est pas un texte`);
-        return ;
+    else if (faillingDone(done))
+    {
+        rpcr.status = 400 ;
+        rpcr.message = `${done} n'est pas un booleen` ;
     }
+    else {
 
-
-    try {
-        let data
-        
-        // Exécution de la bonne requête en fonction des paramètres
-        if (message !== undefined && done !== undefined) {
-            data = await client.query('UPDATE tickets SET message = $1, done = $2 WHERE id = $3 RETURNING *', [message, done, id]);
-        } else if (message !== undefined){
-            data = await client.query('UPDATE tickets SET message = $1 WHERE id = $2 RETURNING *', [message, id]);
-        } else {
-            data = await client.query('UPDATE tickets SET done = $1 WHERE id = $2 RETURNING *', [done, id]);
+        try 
+        {
+            let data
+            
+            // Exécution de la bonne requête en fonction des paramètres
+            if (message !== undefined && done !== undefined) 
+            {
+                data = await client.query('UPDATE tickets SET message = $1, done = $2 WHERE id = $3 RETURNING *', [message, done, id]);
+            } 
+            else if (message !== undefined)
+            {
+                data = await client.query('UPDATE tickets SET message = $1 WHERE id = $2 RETURNING *', [message, id]);
+            } 
+            else 
+            {
+                data = await client.query('UPDATE tickets SET done = $1 WHERE id = $2 RETURNING *', [done, id]);
+            }
+    
+            // Vérifiction de l'existence de l'id
+            if (data.rowCount > 0) 
+            {
+                rpcr.status = 200 ;
+                rpcr.message = `Le ticket ${id} a bien été modifier` ;
+                rpcr.data = data.rows[0] ;
+            } 
+            else 
+            {
+                rpcr.status = 404 ;
+                rpcr.message = `Le ticket ${id} n'existe pas` ;
+            }
         }
-
-        // Vérifiction de l'existence de l'id
-        if (data.rowCount > 0) {
-            res.status(200).json({
-                status: "SUCCESS",
-                message: `Le ticket ${id} a bien été modifier`,
-                data: data.rows[0]
-            });
-            console.log(`put | /api/tickets | 200 | SUCCESS\nLe ticket ${id} a bien été modifier`);
-        } else {
-            res.status(400).json({
-                status: "FAIL",
-                message: `Le ticket ${id} n'existe pas`,
-                data : undefined
-            });
-            console.log(`put | /api/tickets | 400 | FAIL\nLe ticket ${id} n'existe pas`);
+        catch (err) 
+        {
+            console.log(err.stack)
         }
     }
-    catch (err) {
-        res.status(500).json({
-            status: "FAIL",
-            message: "Serveur introuvable",
-            data : undefined
-        });
-        console.log(`put | /api/tickets | 500 | FAIL\n${err.stack} `);
-    }
+    console.log(`put | /api/tickets | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
 // Suppression d'un Ticket
-app.delete('/api/tickets/:id', async (req, res) => {
+app.delete('/api/tickets/:id', async (req, res) => 
+{
+    let rpcr = new Responcer()
     const id = req.params.id;
 
     // Vérifiction du Type de l'id entrante
-    if (
-        Number.isNaN(Number(id)) || 
-        id % 1 !== 0 ||
-        typeof id === typeof Boolean()
-    )
+    if (faillingId(id))
     {
-        res.status(404).json({
-            status: "FAIL",
-            message: `${id} n'est pas un nombre entier`,
-            data : undefined
-        });
-        console.log(`delete | /api/tickets/${id} | 404 | FAIL\n${id} n'est pas un nombre`);
-        return ;
+        rpcr.status = 400 ;
+        rpcr.message = `${id} n'est pas un nombre entier` ;
     }
-    try {
-        const data = await client.query('DELETE FROM tickets WHERE id = $1', [id]);
-        
-        // Vérifiction de l'existence de l'id
-        if (data.rowCount === 1) {
-            res.status(200).json({
-                status : "SUCCESS",
-                message: `Le ticket ${id} a bien été supprimé`,
-                data : undefined
-            });
-            console.log(`delete | /api/tickets/${id} | 200 | SUCCESS\nLe ticket ${id} a bien été supprimé`);
-        } else {
-            res.status(404).json({
-                status: "FAIL",
-                message: `Aucune ticket ne correspond à l'id ${id}`,
-                data : undefined
-            });
-            console.log(`delete | /api/tickets/${id} | 404 | FAIL\nAucune ticket ne correspond à l'id ${id}`);
+    else 
+    {
+        try 
+        {
+            const data = await client.query('DELETE FROM tickets WHERE id = $1', [id]);
+            
+            // Vérifiction de l'existence de l'id
+            if (data.rowCount === 1) 
+            {
+                rpcr.status = 200 ;
+                rpcr.message = `Le ticket ${id} a bien été supprimé` ;
+            } 
+            else 
+            {
+                rpcr.status = 404 ;
+                rpcr.message = `Aucune ticket ne correspond à l'id ${id}` ;
+            }
         }
-
+        catch (err) 
+        {
+            console.log(err.stack);
+        }
     }
-    catch (err) {
-        res.status(500).json({
-            status: "FAIL",
-            message: "Serveur introuvable",
-            data : undefined
-        });
-        console.log(`delete | /api/tickets/${id} | 500 | FAIL\n${err.stack} `);
-    }
+    console.log(`delete | /api/tickets/${id} | ${rpcr.info()}`);
+    res = rpcr.send(res)
 })
 
-app.all('*', async (req, res) => {
-    res.status(404).json({
-        status: "FAIL",
-        message: `Cette requête n'existe pas`,
-        data : undefined
-    });
+app.all('*', async (req, res) => 
+{
+    let rpcr = new Responcer({
+        status : 404,
+        message : `Cette requête n'existe pas`
+    })
+    console.log(`* | ${rpcr.info()}`);
+    console.log(req);
+    res = rpcr.send(res)
 });
 
 // ecoute le port 8000
